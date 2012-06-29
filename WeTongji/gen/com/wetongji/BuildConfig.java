21e3b320b45680e921c95b93fb4301eb798b9c6b
Martian <nl.martian@gmail.com>
2012-06-30 02:36:25 +0800

    做了一个初步的欢迎界面 增加了git忽略列表 从仓库中移除了一些不需要跟踪的文件夹

--- WeTongji/gen/com/wetongji/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wetongji;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ WeTongji/src/com/wetongji/AppIndex.java
@@ -0,0 +1,13 @@
+package com.wetongji;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppIndex extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_welcome_view);
+    }
+}
