21a8bd42d5a45ef1fc0fa40e8f76c14283c5e852
TonyHe <coolhzb@163.com>
2012-07-06 00:11:08 +0800

    进一步完善网络层，所有的基本操作全部实现

--- WeTongji/src/com/wetongji/net/WTClient.java
+++ WeTongji/src/com/wetongji/net/WTClient.java
@@ -3,19 +3,35 @@
  */
 package com.wetongji.net;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author hezibo
@@ -27,11 +43,13 @@ public class WTClient
 	
 	private HttpClient httpClient;
 	private HttpGet request;
+	private HttpPost post;
 	
 	private boolean hasError;
 	private String errorDesc;
 	private int responseStatusCode;
 	private boolean sessionRequired;
+	private boolean currentUserIdRequired;
 	
 	private Map<String, String> params;
 	
@@ -42,8 +60,10 @@ public class WTClient
 	{
 		httpClient = new DefaultHttpClient();
 		request = new HttpGet();
+		post = new HttpPost();
 		hasError = false;
 		sessionRequired = false;
+		currentUserIdRequired = false;
 		errorDesc = null;
 		responseStatusCode = 0;
 		params = new HashMap<String, String>();
@@ -123,8 +143,15 @@ public class WTClient
 	//执行request
 	public void executeRequest() throws Exception
 	{
+		if(this.sessionRequired)
+		{
+			params.put("S", "jjglajglagh");
+		}
+		
 		this.buildURL();
 		HttpResponse response = httpClient.execute(request);
+		String responseString = EntityUtils.toString(response.getEntity());
+		JSONObject jsonObject = new JSONObject(responseString);
 	}
 	
 	//激活用户账号
@@ -165,17 +192,36 @@ public class WTClient
 	}
 	
 	//修改用户头像
-	public void updateUserAvatar()
+	public void updateUserAvatar(File imageFile) throws ClientProtocolException, IOException, Exception
 	{
 		params.put("M", "User.Update.Avatar");
 		this.sessionRequired = true;
+		
+		Set<Map.Entry<String, String>> paramsSet = new HashSet<Map.Entry<String, String>>(params.entrySet());
+		UploadImage(APIDomain, paramsSet, imageFile);
+		httpClient.execute(this.post);
 	}
 	
 	//修改用户资料
-	public void updateUser()
+	public void updateUser(String phone, String email, String qq, String weibo) throws JSONException, Exception
 	{
 		params.put("M", "User.Update");
 		this.sessionRequired = true;
+		
+		JSONObject json = new JSONObject();
+		
+		json.put("Email", email);
+		json.put("SinaWeiBo", weibo);
+		json.put("Phone", phone);
+		json.put("QQ", qq);
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("User", json);
+		System.out.println("josnStr:"+json.toString());
+		
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("jsonString",jsonObject.toString()));
+		post.setEntity(new UrlEncodedFormEntity(pairs));
 	}
 	
 	//获取用户信息
@@ -183,5 +229,143 @@ public class WTClient
 	{
 		params.put("M", "User.Get");
 		this.sessionRequired = true;
+	}
+	
+	//用来上传图片
+	public void UploadImage(String basicURL, Set<Map.Entry<String, String>> params, File imageFile) throws Exception
+	{
+		String url = basicURL;
+		post.setURI(new URI(url));
+		
+		 //设置MultipartEntity的模式为BROWSER_COMPATIBLE
+		 MultipartEntity multiEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+		 
+		 //图片内容
+		 FileBody binaryContent = new FileBody(imageFile);
+         multiEntity.addPart("Image", binaryContent);
+         
+         //构造表单参数
+         for(Map.Entry<String, String> param : params) 
+         {
+
+         	multiEntity.addPart(param.getKey(),
+         			new StringBody(param.getValue()));
+         }
+         
+         post.setEntity(multiEntity);
+	}
+	
+	//获取校园新闻列表
+	public void getNewsList(int page) throws Exception
+	{
+		params.put("M", "News.GetList");
+		if(page <= 0)
+			page = 1;
+		String pageStr = String.valueOf(page);
+		params.put("P", pageStr);
+		this.executeRequest();
+	}
+	
+	//获取某个频道的活动列表
+	public void getActivitiesWithChannelIds(String channelId, String sortType, int page)
+	{
+		
+	}
+	
+	//喜欢某个活动
+	public void likeActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.Like");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//添加活动到日程列表
+	public void scheduleActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.Schedule");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//关注某个活动
+	public void favoriteActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.Favorite");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//取消喜欢某个活动
+	public void unlikeActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.UnLike");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//从日程删除某个活动
+	public void unscheduleActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.UnScheduleLike");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//取消关注某个活动
+	public void unfavoriteActivity(String activityId) throws Exception
+	{
+		params.put("M", "Activity.UnFavoriteLike");
+		params.put("Id", activityId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//获取课程表
+	public void getCourse() throws Exception
+	{
+		params.put("M", "TimeTable.Get");
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//喜欢某个课程
+	public void likeCourse(String courseId) throws Exception
+	{
+		params.put("M", "Course.Like");
+		params.put("Id", courseId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//取消喜欢某个课程
+	public void unlikeCourse(String courseId) throws Exception
+	{
+		params.put("M", "Course.UnLike");
+		params.put("Id", courseId);
+		this.sessionRequired = true;
+		this.executeRequest();
+	}
+	
+	//
+	public void getFavoriteList() throws Exception
+	{
+		params.put("M", "Favorite.Get");
+		this.sessionRequired = true;
+		this.currentUserIdRequired = true;
+		this.executeRequest();
+	}
+	
+	//
+	public void readNews(String newsId) throws Exception
+	{
+		params.put("M", "News.Get");
+		params.put("Id", "newsId");
+		this.executeRequest();
 	}
 }
