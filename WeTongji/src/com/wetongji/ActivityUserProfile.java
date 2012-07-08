4c0542997cfd2e758b162c0a122ab0db6345a0a6
Martian <nl.martian@gmail.com>
2012-07-09 00:55:43 +0800

    解决4.0注册登录输入框看不见文字bug。个人资料Activity增加Edit MenuItem

--- WeTongji/src/com/wetongji/ActivityUserProfile.java
+++ WeTongji/src/com/wetongji/ActivityUserProfile.java
@@ -8,6 +8,7 @@ package com.wetongji;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 /**
@@ -27,14 +28,30 @@ public class ActivityUserProfile extends SherlockActivity {
     }
     
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add("Edit")
+            .setIcon(R.drawable.ic_compose)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
+    }
+    
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         case android.R.id.home:
             finish();
             return true;
-        default:
-            return super.onOptionsItemSelected(item);
         }
+        if(item.getTitle().equals("Edit")) {
+            // ½øÈë±à¼­Ò³Ãæ
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+    
+    
 
 }
